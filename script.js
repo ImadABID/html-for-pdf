53ba74538fccae623fd6c94057e973a0d4f46841
ImadAbid <imad.abied@gmail.com>
2022-09-17 17:24:58 +0200

    adding a function for page alignement debug

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const alter = ()=>{
-    let div = document.getElementById('the_unique_div')
-    div.innerHTML = "JavaScript"
+
+/* Debug */
+const debug_page_alignment = ()=>{
+    let i = 0;
+    let pages = document.getElementsByClassName('page_A4');
+    
+    Array.prototype.filter.call(
+        pages,
+        (page) => {
+            if(i%2 == 0){
+                page.style['background-color'] = 'rgb(247, 137, 137)';
+            }else{
+                page.style['background-color'] = 'rgb(137, 247, 165)';
+            }
+            i++;
+        }
+    );
 }
